19c287a7e01e3f72000dda592d586c08abf7c46b
faisalKhan <faisaldev404@gmail.com>
2025-05-21 19:40:06 +0500

    add: added valiation to the sending request to the another user.

--- controller/request.Controller.js
+++ controller/request.Controller.js
@@ -1,45 +1,61 @@
 const { ConnectionRequest } = require("../model/connectionRequest.model");
-
-
-
+const { userModel } = require("../model/user.model");
 
 async function requestSend(req, res) {
-
     try {
-        const receiverId = req.userId;
-
-        const senderId = req.params.senderId;
-
+        const senderId = req.userId;
+        const receiverId = req.params.receiverId;
         const status = req.params.status;
 
+        // Allowed status to be send.
+        const allowedStatus = ["ignored", "interested"];
+
+        if (!allowedStatus.includes(status)) {
+            return res.status(400).json({
+                message: "Invalid status type: " + status,
+            });
+        }
+
+        // checking whether the reciver present in the Database.
+
+        const existReciver = await userModel.findById(receiverId);
+        if (!existReciver) {
+            return res.status(400).json({
+                message: "Reciver user doesn't exist."
+            });
+        }
+
+        // Can't send the request twice/2 times.
+        const existRequest = await ConnectionRequest.findOne({
+            senderId,
+            receiverId,
+        });
+
+        if (existRequest) {
+            res.status(400).json({
+                message: "Connection request already sent.",
+            });
+        }
 
         const connectionRequest = await new ConnectionRequest({
-            receiverId,
             senderId,
-            status
-        })
+            receiverId,
+            status,
+        });
 
         const requestData = await connectionRequest.save();
 
         res.status(200).json({
             message: "the connection has been send successfully.",
-            requestData
-        })
-
-
+            requestData,
+        });
     } catch (error) {
         res.json({
-            error: error.message
-        })
+            error: error.message,
+        });
     }
-
-
-
 }
-
-
-
 
 module.exports = {
-    requestSend
-}
+    requestSend,
+};
