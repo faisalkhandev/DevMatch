b1673f82edaecac08d4db87b3516803ef8f5b689
faisalKhan <faisaldev404@gmail.com>
2025-05-26 10:25:27 +0500

    fix: update requestRouter to correctly handle response requests with requestRespond function

--- routes/requestRoute.js
+++ routes/requestRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { userAuth } = require("../middleware/userAuth.middleware");
-const { requestSend } = require("../controller/request.Controller");
+const { requestSend, requestRespond } = require("../controller/request.Controller");
 
 const requestRouter = express.Router();
 
 requestRouter.post("/request/send/:status/:receiverId", userAuth, requestSend)
 
-requestRouter.post("/request/:status/:requestId", userAuth, requestSend)
+requestRouter.post("/request/respond/:status/:requestId", userAuth, requestRespond)
 
 
 module.exports = {
