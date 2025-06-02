304f26c93871778435ed34dc4cdaef300262fd75
faisalKhan <faisaldev404@gmail.com>
2025-06-02 12:25:59 +0500

    sign up and login page done.

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React from 'react';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>Sign Uppp</h1>
-        </div>
-    )
-}
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-white">
+                    Create a new account
+                </h2>
+            </div>
 
-export default SignUp
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form action="#" method="POST" className="space-y-6">
+                    <div>
+                        <label htmlFor="firstName" className="block text-sm font-medium text-white">
+                            First Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="firstName"
+                                name="firstName"
+                                type="text"
+                                required
+                                autoComplete="given-name"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="lastName" className="block text-sm font-medium text-white">
+                            Last Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="lastName"
+                                name="lastName"
+                                type="text"
+                                required
+                                autoComplete="family-name"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-white">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                autoComplete="email"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium text-white">
+                            Password
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                autoComplete="new-password"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign up
+                        </button>
+                    </div>
+                </form>
+
+                {/* Login message */}
+                <div className="mt-6 text-center text-sm text-white">
+                    Already have an account?{' '}
+                    <a href="/login" className="font-semibold text-blue-500 hover:text-blue-600">
+                        Log in
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
