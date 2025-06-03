1cac5e0a29e2ad481c03d6066c6ccb5e4a9126e3
faisalKhan <faisaldev404@gmail.com>
2025-06-03 11:32:39 +0500

    add: implement user data fetching in Body component with error handling and navigation

--- frontend/src/Pages/Body.jsx
+++ frontend/src/Pages/Body.jsx
@@ -1,7 +1,40 @@
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
 import { Footer, Header } from '../Components'
+import axios from 'axios';
+import { BASE_URL } from '../utils/constant';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../Store/slice/authSlice';
+import { showToast } from '../Components/ShowToast';
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.user);
+
+    async function fetchUser() {
+        try {
+
+            const res = await axios.get(BASE_URL + '/api/v1/profile/view', {
+                withCredentials: true
+            })
+            dispatch(addUser(res.data.user));
+        } catch (error) {
+            if (error.status === 401) {
+                navigate('/login');
+                return;
+            }
+            showToast('Failed to fetch user data. Please try login again.', 'error');
+        }
+
+    }
+
+    useEffect(() => {
+        if (!userData) {
+            fetchUser();
+        }
+    }, [])
+
     return (
         <div>
             <Header />
