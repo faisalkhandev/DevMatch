29cc4dba76cc06153b67bacbc829d9d8c6335336
faisalKhan <faisaldev404@gmail.com>
2025-06-12 19:21:45 +0500

    changes.

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.DB_CONNECTION_SECRET);
-        console.log("DB ENV:::", process.env.DB_CONNECTION_SECRET)
+        await mongoose.connect("mongodb+srv://devMatch:f9ZKaEo3u4HEhStY@cluster0.iv8geja.mongodb.net/");
+
         console.log("MongoDB connected successfully!");
     } catch (error) {
         console.error("MongoDB connection error:", error);
