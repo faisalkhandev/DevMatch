e127f7452a25eeee49c728c014c9272019228773
faisalKhan <faisaldev404@gmail.com>
2025-06-12 11:48:21 +0500

    Version 1.0 Done.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -67,6 +67,10 @@ const Header = () => {
                     </div>
                 </Link>
 
+                {<>
+                    <span>{(user?.firstName + " " + user?.lastName).toUpperCase()}</span>
+                </>
+                }
                 {/* User Dropdown */}
                 {user?.photoUrl && (
                     <div className="dropdown dropdown-end">
@@ -80,6 +84,7 @@ const Header = () => {
                             tabIndex={0}
                             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
                         >
+
                             <li>
                                 <Link to="/profile" className="justify-between">
                                     Profile
