9f0ca163f76ae0ee1a3be227042e3a4c772687f3
faisalKhan <faisaldev404@gmail.com>
2025-06-16 15:38:26 +0500

    add: cornJob for yesterday requests.

--- backend/utils/cronJob.js
+++ backend/utils/cronJob.js
@@ -1,5 +1,52 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+const { ConnectionRequest } = require("../model/connectionRequest.model");
+const { sendTemplatedEmail } = require("./sendEmail");
 
-cron.schedule('* * * * *', () => {
-    console.log('running a task every minute' + new Date());
+cron.schedule("0 16 * * *", async () => {
+    const yesterday = subDays(new Date(), 1);
+    const startOfYesterday = startOfDay(yesterday);
+    const endOfYesterday = endOfDay(yesterday);
+
+    try {
+        const interestedRequestsFromYesterday = await ConnectionRequest.find({
+            status: "interested",
+            createdAt: {
+                $gte: startOfYesterday,
+                $lt: endOfYesterday,
+            },
+        }).populate([
+            { path: "senderId", select: "firstName lastName emailId" },
+            { path: "receiverId", select: "firstName lastName emailId" },
+        ]);
+
+        console.log(
+            "Interested requests from yesterday:",
+            interestedRequestsFromYesterday.length
+        );
+
+        for (const request of interestedRequestsFromYesterday) {
+            const receiverEmail = request.receiverId?.emailId;
+            const senderName = `${request.senderId?.firstName} ${request.senderId?.lastName}`;
+            const receiverName = `${request.receiverId?.firstName} ${request.receiverId?.lastName}`;
+
+            if (!receiverEmail) continue;
+
+            try {
+                await sendTemplatedEmail({
+                    to: receiverEmail,
+                    from: 'khanfai900@gmail.com',
+                    senderName,
+                    receiverName,
+                    status: request.status,
+                });
+
+                console.log(`Email sent to ${receiverEmail}`);
+            } catch (err) {
+                console.error(`Failed to send email to ${receiverEmail}:`, err.message);
+            }
+        }
+    } catch (error) {
+        console.log("Error in scheduled task:", error);
+    }
 });
