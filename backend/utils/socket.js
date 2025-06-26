9c7b8a494d47ce31e390c056aee198affd7e8002
faisalKhan <faisaldev404@gmail.com>
2025-06-26 20:24:16 +0500

    add: added the crypto(assign random long string) to the room ids.

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -1,4 +1,14 @@
 const socket = require("socket.io");
+const crypto = require("crypto")
+
+
+const getSecretRoomId = (senderId, receiverId) => {
+    return crypto
+        .createHash("sha256")
+        .update([senderId, receiverId].sort().join("$"))
+        .digest("hex");
+};
+
 
 const initializeSocket = (server) => {
     const io = socket(server, {
@@ -11,13 +21,14 @@ const initializeSocket = (server) => {
 
         //user can join the chat.
         socket.on("joinChat", ({ senderId, receiverId, firstName, lastName }) => {
-            const roomId = [senderId, receiverId].sort().join("-");
+            const roomId = getSecretRoomId(senderId, receiverId)
+            console.log("roomID::", roomId)
             socket.join(roomId);
         });
 
         //user can send the message.
         socket.on("sendMessage", ({ firstName, senderId, receiverId, text, time }) => {
-            const roomId = [senderId, receiverId].sort().join("-");
+            const roomId = getSecretRoomId(senderId, receiverId)
             console.log(firstName + " says " + text, time)
             io.to(roomId).emit("receiveMessage", { text, time, firstName, senderId, receiverId });
         });
