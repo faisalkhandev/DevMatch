c184588c7301d68a40657b95b9c0a091de472402
faisalKhan <faisaldev404@gmail.com>
2025-06-26 17:39:50 +0500

    refactor the code.

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -10,21 +10,23 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
 
         //user can join the chat.
-        socket.on("joinChat", ({ userId, targetUserId, firstName, lastName }) => {
-            const roomId = [userId, targetUserId].sort().join("-");
-            console.log(firstName, lastName + "joining the room", roomId,)
+        socket.on("joinChat", ({ senderId, receiverId, firstName, lastName }) => {
+            const roomId = [senderId, receiverId].sort().join("-");
             socket.join(roomId);
         });
 
         //user can send the message.
-        socket.on("sendMessage", (message) => {
-            io.emit("message", message);
+        socket.on("sendMessage", ({ firstName, senderId, receiverId, text, time }) => {
+            const roomId = [senderId, receiverId].sort().join("-");
+            console.log(firstName + " says " + text, time)
+            io.to(roomId).emit("receiveMessage", { text, time, firstName, senderId, receiverId });
         });
 
         //user disconnect the socket.
         socket.on("disconnect", () => {
-            console.log("A user disconnected");
+            console.log(`❌ User disconnected: ${socket.id}`);
         });
+
     });
 };
 
