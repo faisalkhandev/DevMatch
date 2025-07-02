4fd25ce7261b05c5efcfbd00c1b5b705b9f3e95d
faisalKhan <faisaldev404@gmail.com>
2025-07-02 19:13:33 +0500

    testing v2.3

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,15 +1,13 @@
-import io from 'socket.io-client';
+import io from 'socket.io-client'
 import { BASE_URL } from './constant';
+
+
 
 export function createSocketConnection() {
     if (location.hostname === "localhost") {
-        return io(BASE_URL, {
-            transports: ["websocket"],
-        });
-    } else {
-        return io("/", {
-            path: "/socket.io",
-            transports: ["websocket"],
-        });
+        return io(BASE_URL)
+    }
+    else {
+        return io("/", { path: "/socket.io" })
     }
 }
